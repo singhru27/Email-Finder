b81073b1f564596493b6d46222511e8ce6572af8
Rujul Singh <rujulsingh@Rujuls-MacBook-Pro.local>
2022-12-25 21:58:02 -0500

    Command Line version complete

--- email_finder.py
+++ email_finder.py
@@ -1,13 +1,14 @@
 import csv
 import config
+import json
 from validate_email import MailtesterSingle
 
 
 class EmailWriter():
     def __init__(self):
-        pass
+        self.apiCounter = 0
 
-    def getEmailFormat(self, firstName, lastName, domain):
+    def getEmailFormat(self, firstName, lastName, company, domain, role):
 
         if not firstName or not lastName or not domain:
             return None
@@ -19,45 +20,58 @@ class EmailWriter():
         emailList.append(firstName[0] + "." + lastName + "@" + domain)
         emailList.append(lastName + firstName + "@" + domain)
         emailList.append(lastName + "." + firstName[0] + "@" + domain)
-        emailList.append(firstName + "-" + lastName + "@" + domain)
         emailList.append(firstName + "_" + lastName + "@" + domain)
         emailList.append(firstName[0] + "_" + lastName + "@" + domain)
         emailList.append(firstName + lastName[0] + "@" + domain)
         emailList.append(lastName + "." + firstName + "@" + domain)
         emailList.append(firstName + "@" + domain)
-        emailList.append(lastName + "@" + domain)
-        emailList.append(firstName[0] + lastName[0] + "@" + domain)
         emailList.append(firstName + "." + lastName[0] + "@" + domain)
         emailList.append(lastName + firstName[0] + "@" + domain)
         emailList.append(lastName[0] + firstName + "@" + domain)
-        emailList.append(lastName[0] + "." + firstName + "@" + domain)
         emailList.append(firstName[0] + "-" + lastName + "@" + domain)
         emailList.append(firstName + "-" + lastName[0] + "@" + domain)
         emailList.append(lastName + "-" + firstName + "@" + domain)
         emailList.append(lastName + "_" + firstName + "@" + domain)
-        emailList.append(lastName[0] + "_" + firstName + "@" + domain)
+        emailList.append(firstName + "-" + lastName + "@" + domain)
 
         for email in emailList:
-            print(email)
+            self.apiCounter += 1
             E = MailtesterSingle(config.API_KEY, email)
             res = E.control()
-            break
-        
-        return res
+            res = json.loads(res)
+            print(email)
+            print(res)
+            if res["result"] == "valid":
+                self.writeToCSV(firstName, lastName, company, domain, role, email)
+                return True
+            elif res["result"] == "unknown" or res["result"] == "risky":
+                self.writeToCSV(firstName, lastName, company, domain, role, email, "x")
+                return True
 
+        return False
+
+    def writeToCSV(self, firstName, lastName, company, domain, role, email, tag=None):
+
+        with open("emails.csv", "a") as destinationFile:
+            emailWriter = csv.writer(destinationFile)
+            if tag:
+                emailWriter.writerow([firstName + " " + lastName, company, domain, role, email, "x"])
+            else:
+                emailWriter.writerow([firstName + " " + lastName, company, domain, role, email])
+        
+    def convertCSV(self):
+        with open("source.csv") as sourceFile:
+            emailReader = csv.reader(sourceFile)
+            for i, row in enumerate(emailReader):
+                name = row[0].split()
+                firstName = name[0]
+                lastName = name[1]
+                company = row[1]
+                domain = row[2]
+                role = row[3]
+                self.getEmailFormat(firstName, lastName, company, domain, role)
+                print(self.apiCounter)
 
 if __name__ == "__main__":
     EmailWriter = EmailWriter()
-    print("Enter First Name")
-    firstName = input()
-    print("Enter Last Name")
-    lastName = input()
-    print("Enter Domain")
-    domain = input()
-
-    result = EmailWriter.getEmailFormat(firstName, lastName, domain)
-
-    if result:
-        print(result)
-    else:
-        print("No email found")
+    EmailWriter.convertCSV()
