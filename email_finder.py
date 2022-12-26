1555bce482e3678de681a267d757eaee7ce8b22d
Rujul Singh <rujulsingh@Rujuls-MacBook-Pro.local>
2022-12-25 19:42:47 -0500

    Integrated with Mailtester API

--- email_finder.py
+++ email_finder.py
@@ -1,4 +1,6 @@
 import csv
+import config
+from validate_email import MailtesterSingle
 
 
 class EmailWriter():
@@ -11,13 +13,51 @@ class EmailWriter():
             return None
 
         emailList = []
-        emailList.append(firstName + lastName + domain)
-        emailList.append(firstName + "." + lastName + domain)
-        emailList.append(firstName[0] + lastName + domain)
-        emailList.append(firstName + lastName[0] + domain)
-        emailList.append(firstName + domain)
-        emailList.append(lastName + domain)
-        emailList.append(firstName[0] + "." + lastName + domain)
-        emailList.append(firstName + "." + lastName[0] + domain)
-        emailList.append(lastName + firstName + domain)
-        emailList.append(lastName + "." + firstName + domain)
+        emailList.append(firstName + lastName + "@" + domain)
+        emailList.append(firstName + "." + lastName + "@" + domain)
+        emailList.append(firstName[0] + lastName + "@" + domain)
+        emailList.append(firstName[0] + "." + lastName + "@" + domain)
+        emailList.append(lastName + firstName + "@" + domain)
+        emailList.append(lastName + "." + firstName[0] + "@" + domain)
+        emailList.append(firstName + "-" + lastName + "@" + domain)
+        emailList.append(firstName + "_" + lastName + "@" + domain)
+        emailList.append(firstName[0] + "_" + lastName + "@" + domain)
+        emailList.append(firstName + lastName[0] + "@" + domain)
+        emailList.append(lastName + "." + firstName + "@" + domain)
+        emailList.append(firstName + "@" + domain)
+        emailList.append(lastName + "@" + domain)
+        emailList.append(firstName[0] + lastName[0] + "@" + domain)
+        emailList.append(firstName + "." + lastName[0] + "@" + domain)
+        emailList.append(lastName + firstName[0] + "@" + domain)
+        emailList.append(lastName[0] + firstName + "@" + domain)
+        emailList.append(lastName[0] + "." + firstName + "@" + domain)
+        emailList.append(firstName[0] + "-" + lastName + "@" + domain)
+        emailList.append(firstName + "-" + lastName[0] + "@" + domain)
+        emailList.append(lastName + "-" + firstName + "@" + domain)
+        emailList.append(lastName + "_" + firstName + "@" + domain)
+        emailList.append(lastName[0] + "_" + firstName + "@" + domain)
+
+        for email in emailList:
+            print(email)
+            E = MailtesterSingle(config.API_KEY, email)
+            res = E.control()
+            break
+        
+        return res
+
+
+if __name__ == "__main__":
+    EmailWriter = EmailWriter()
+    print("Enter First Name")
+    firstName = input()
+    print("Enter Last Name")
+    lastName = input()
+    print("Enter Domain")
+    domain = input()
+
+    result = EmailWriter.getEmailFormat(firstName, lastName, domain)
+
+    if result:
+        print(result)
+    else:
+        print("No email found")
---
+++ validate_email.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python 
+# _*_ coding:utf-8 _*_
+
+
+import requests
+import datetime
+
+
+class MailtesterSingle():
+   
+    def __init__(self, key, email):
+        self.key = key
+        self.email = email
+        self.verif = "https://api.mailtester.com/api/singlemail?secret="
+        self.url = self.verif+self.key+"&email="+self.email
+
+    
+    def control(self):
+        r = requests.get(self.url)
+        return r.text
+
+
+class MailtesterBulk():
+
+    def __init__(self, key, user_file):
+        datenow = datetime.datetime.now()
+        self.key = key
+        self.name = 'File' + datenow.strftime("%Y-%m-%d %H:%M")
+        self.user_file = user_file
+        self.url = 'https://api.mailtester.com/api/bulk?secret='+key+'&filename=%s' % self.name
+
+
+    def upload(self):
+        import pycurl
+        
+        infile = open('id_file', 'w')
+        c = pycurl.Curl()
+        c.setopt(c.POST, 1)
+        c.setopt(c.URL, self.url)
+        c.setopt(c.HTTPPOST, [('file_contents', (
+                    c.FORM_FILE, self.user_file,
+                    c.FORM_CONTENTTYPE, 'text/plain',
+                    c.FORM_FILENAME, self.name.replace(' ','_'),)),])
+        c.setopt(c.WRITEFUNCTION, infile.write)
+        c.setopt(c.VERBOSE, 1)
+        c.perform()
+        c.close()
+
+
+    def get_info(self):
+       
+        with open('id_file','r') as f:
+            ids = f.read()
+        url = 'https://api.mailtester.com/api/details?secret='+self.key+'&id=%s' % ids
+        r = requests.get(url)
+        with open('result.txt', 'a') as res:
+            res.write(r.content+'\n')
+
+
+if __name__ == '__main__':
+    pass
