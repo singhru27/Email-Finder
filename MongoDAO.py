95a0d5b5a073fafb6e04940d3c6738b26ade40a0
Rujul Singh <rujulsingh@Rujuls-MacBook-Pro.local>
2022-12-27 00:18:28 -0500

    Adding logic for user database and email format database

--- MongoDAO.py
+++ MongoDAO.py
@@ -1,9 +1,8 @@
 from pymongo import MongoClient
 import config
-
 PASSWORD = config.MONGO_PASS
 
-class MongoDao():
+class MongoFormatDao():
 
     def __init__(self):
         self.client = self.getDatabase(self.getDatabase())
@@ -14,14 +13,40 @@ class MongoDao():
         client = MongoClient("mongodb+srv://singhru:"+config.MONGO_PASS+"@emailservice.bodim.mongodb.net/?retryWrites=true&w=majority")
         return client
 
-    def getCollection(self, collectionName):
-        collection = self.client[collectionName]
+    def getCollection(self):
+        collection = self.client["Email_Keys"]
         return collection
 
     def insertOne(self, domain, formatID):
         item = {
             "domain" : domain,
             "formatID": formatID
+        }
+        self.collection.insert_one(item)
+
+class MongoUserDAO():
+    def __init__(self):
+        self.client = self.getDatabase(self.getDatabase())
+        self.collection = self.getCollection(self.getCollection())
+        pass
+
+    def getDatabase(self):
+        client = MongoClient("mongodb+srv://singhru:"+config.MONGO_PASS+"@emailservice.bodim.mongodb.net/?retryWrites=true&w=majority")
+        return client
+
+    def getCollection(self):
+        collection = self.client["User Database"]
+        return collection
+
+    def insertOne(self, firstName, lastName, company, domain, role, email, unknownStatus):
+        item = {
+            "firstName" : firstName,
+            "lastName": lastName,
+            "company": company,
+            "domain": domain,
+            "role": role,
+            "email": email,
+            "unknownStatus": unknownStatus
         }
         self.collection.insert_one(item)
 
